eabab9edb0956433bb67d9b1b76dd97da466b47b
fcurella <flavio.curella@gmail.com>
2020-09-30 14:46:18 -0500

    configure git user

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,3 +1,5 @@
+git config --global user.email "actions@github.com"
+git config --global user.name "github actions"
 bump2version ${INPUT_PART}
 
 git remote set-url origin https://${GITHUB_ACTOR}:${INPUT_GITHUBTOKEN}@github.com/${GITHUB_REPOSITORY}.git
