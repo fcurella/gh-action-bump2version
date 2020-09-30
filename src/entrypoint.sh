14148b808d08c276a1c87c6810f6f949a90d85df
fcurella <flavio.curella@gmail.com>
2020-09-30 14:49:33 -0500

    push tags too

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -3,4 +3,4 @@ git config --global user.name "github actions"
 bump2version ${INPUT_PART}
 
 git remote set-url origin https://${GITHUB_ACTOR}:${INPUT_GITHUBTOKEN}@github.com/${GITHUB_REPOSITORY}.git
-git push origin
+git push origin && git push origin --tags
