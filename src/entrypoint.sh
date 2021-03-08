69b040a65f085c7cf65a04f1c9e6642e0f296143
Flavio Curella <89607+fcurella@users.noreply.github.com>
2021-03-08 10:35:52 -0600

    Update entrypoint.sh

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,5 +1,5 @@
 set -e
 
-git config --global user.email "actions@github.com"
+git config --global user.email "actions@users.noreply.github.com"
 git config --global user.name "github actions"
 bump2version ${INPUT_PART}
